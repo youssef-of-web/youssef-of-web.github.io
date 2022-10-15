56d7ec56a75b27dc8949b1797aef08f486372d00
mansouri youssef <mansouriyoussef1991@gmail.com>
2022-10-15 16:15:35 +0100

    add nprogress

---
+++ nprogress.js
@@ -0,0 +1,21 @@
+$(function () {
+  NProgress.configure({
+    minimum: 0.08,
+    easing: "ease",
+    positionUsing: "",
+    speed: 2000,
+    trickle: true,
+    trickleSpeed: 200,
+    showSpinner: false,
+    barSelector: '[role="bar"]',
+    spinnerSelector: '[role="spinner"]',
+    parent: "body",
+    template:
+      '<div class="bar" role="bar"><div class="peg"><img src="./assets/picachu.gif" width="50px"/></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>',
+  });
+  NProgress.start();
+
+  $(document).ready(function () {
+    NProgress.done();
+  });
+});
